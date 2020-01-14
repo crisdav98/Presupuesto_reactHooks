db5e7d80c9996078010daf4ab3235e7b0701e477
crisdav98 <47307974+crisdav98@users.noreply.github.com>
2020-01-13 23:08:42 -0500

    Add files via upload

---
+++ build/precache-manifest.6729f2eb6e2e7ba6dffb658a1920e825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bdd13dd64be5ec9691b795d3f02e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81866b6e6022f8397b74",
+    "url": "/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "ff4210eb9b4d08d37118",
+    "url": "/static/js/2.fb194e7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fb194e7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81866b6e6022f8397b74",
+    "url": "/static/js/main.85ff838e.chunk.js"
+  },
+  {
+    "revision": "837e7e1aa6c139a60345",
+    "url": "/static/js/runtime-main.354188e8.js"
+  }
+]);
